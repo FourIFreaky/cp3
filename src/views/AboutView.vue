972515dde3a5d6a822b9d814f33afeee61b21ce2
Justin Ponton <ponton.justin@gmail.com>
2022-03-21 11:21:34 -0600

    Bug fix

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Founded yesterday to fulfill a very specific need, we are determined to fill that need in at least some way!</p>
+    <p>If you wish to contact us, here is in information:</p>
+    <p>Address: We Are Everywhere</p>
+    <p>Email:itsTooLate@doom.edu</p>
+    <p>Phone Number: (666)444-1313</p>
   </div>
 </template>
